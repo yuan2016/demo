ff5801fc7b4d4e92c574f56f7576b191ed0e5835
gao.yuan <546961081@qq.com>
2017-06-27 19:01:44 +0800

    提交

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '../App'
+
+const home = r => require.ensure([], () => r(require('../components/home/home')), 'home')
+const data = r => require.ensure([], () => r(require('../components/data/data')), 'data')
+
+Vue.use(Router)
+
+export default [{
+  path: '/',
+  component: App,
+  children: [{path: '', redirect: '/home'},
+    { path: '/home', component: home },
+    { path: '/data', component: data }
+  ]
+}];
